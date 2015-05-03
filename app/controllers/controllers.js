e30fd2a2b329d9569328acd7786171b620651bfb
oliver <Oliver@dokoliho.de>
2015-05-03 23:41:57 +0200

    Game added

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -41,9 +41,11 @@ app.controller('SessionController', function($scope, Session) {
 })
 
 
-app.controller('GameController', function($scope) {
+app.controller('GameController', function($scope, Game) {
   $scope.games =  [
       { id: 1, session: 1, lead: [0], runnings: 0, factor: 1, won: true, kind: "SOLO" },
       { id: 2, session: 1, lead: [1, 2], runnings: 1, factor: 1, won: true, kind: "RUF" }
     ];
+  $scope.types = Game.AllTypes;
+
   })
---
+++ app/factories/game.js
@@ -0,0 +1,121 @@
+app.factory("Game", function($http) {
+
+
+  var Game = function(id, session, type, nonplayers, won, tailor, black, runnings, shots) {
+
+     var BASE_VALUE = 10;
+     var RUN_VALUE = 5;
+
+  	 var _nonplayers = [];
+  	 var _runnings = 0;
+  	 var _black = false;
+     var _tailor = false;
+     var _shots = 0;
+     var _lost = false;
+     var _type;
+     var _session;
+     var _id;
+
+     this.initialize = function() {
+       var self = this;
+       _nonplayers = nonplayers;
+       _runnings = runnings;
+       _black = black;
+       _tailor = tailor;
+       _shots = shots;
+       _lost = !won;
+       _type = type;
+       _session = session;
+  	   _id = id;
+     };
+
+     this.totalValue = function() {
+       var result = _type.value * BASE_VALUE;
+       if (_type.doubleable) {
+         if (_tailor) result += BASE_VALUE;
+         if (_black) result += BASE_VALUE; 
+       }
+       if (_type.runnable) {
+         result += RUN_VALUE * _runnings; 
+       }
+       if (_type.doubleable) {
+         result *= (1 + shots);
+       }
+       result *= _type.factor;
+       return result * _type.nonPlayerCount;
+     }
+
+     this.initialize();
+
+     return {
+      GAME_RUF: function() { return types[0]; },
+
+     	getGameType: function() { return _type; },
+      isWon: function() { return !_lost; },
+     	isTailor: function() { return _tailor; },
+      isBlack: function() { return _black; },
+      getRunnings: function() { return _runnings; },
+      getShots: function() { return _shots; },
+      getSession: function() { return _session; },
+     	id: function() { return _id; },
+      getValue: function() { return totalValue(); }
+     } 
+  };
+
+  Game.AllTypes = [
+    {
+      "name" : "Ruf",
+      "value" : 1,
+      "nonPlayerCount" : 2,
+      "runnable" : true,
+      "doubleable" : true,
+      "factor" : 1
+    },
+    {
+      "name" : "Ramsch",
+      "value" : 1,
+      "nonPlayerCount" : 3,
+      "runnable" : false,
+      "doubleable" : false,
+      "factor" : 1
+    },
+    {
+      "name" : "Wenz",
+      "value" : 2,
+      "nonPlayerCount" : 3,
+      "runnable" : true,
+      "doubleable" : true,
+      "factor" : 1
+    },
+    {
+      "name" : "Solo",
+      "value" : 2,
+      "nonPlayerCount" : 3,
+      "runnable" : true,
+      "doubleable" : true,
+      "factor" : 1
+    },
+    {
+      "name" : "Tout",
+      "value" : 2,
+      "nonPlayerCount" : 3,
+      "runnable" : true,
+      "doubleable" : true,
+      "factor" : 2
+    },
+    {
+      "name" : "Sie",
+      "value" : 2,
+      "nonPlayerCount" : 3,
+      "runnable" : true,
+      "doubleable" : true,
+      "factor" : 4
+    }
+  ]
+
+  return (Game);
+
+});
+
+
+
